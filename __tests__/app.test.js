4aa304beec3cc5ee06bb78446046aa093cbbf62b
Lee Roberts <lee@Lees-MacBook-Air.local>
2022-12-12 12:59:15 +0000

    this commit will create a test file for TDD on requests

---
+++ __tests__/app.test.js
@@ -0,0 +1,23 @@
+const request = require("supertest");
+const app = require("../app");
+const db = require("../db/connection");
+const seed = require("../db/seeds/seed");
+const testData = require("../db/data/test-data");
+
+beforeEach(() => seed(testData));
+
+afterAll(() => {
+  db.end();
+});
+
+describe.only("GET /api/topics", () => {
+  test("status:200, responds with an array of topic objects", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then(({ body }) => {
+        const { topics } = body;
+        expect(topics).toBeInstanceOf(Array);
+      });
+  });
+});
